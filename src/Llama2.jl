338de4444711424012b34c9eaf9c73b68674c66b
Lukas Mayrhofer <lukas.mayrhofer@tum.de>
2024-07-18 20:24:06 +0200

    Improve accuracy of vecdot routines

--- src/Llama2.jl
+++ src/Llama2.jl
@@ -15,12 +15,7 @@ export train
 export sampleObs
 
 # quantization
-include("quantization/utils.jl")
-include("quantization/common.jl")
-include("quantization/q4.jl")
-include("quantization/q6.jl")
-include("quantization/q8.jl")
-include("quantization/vecdot.jl")
+include("kquants.jl")
 
 # inference
 include("tokenizer.jl")
